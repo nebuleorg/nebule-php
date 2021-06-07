749d5675285bd26e08e907656bd8d7e3dd841278
nebuleorg <deisto@nebule.org>
2021-06-07 20:43:41 +0200

    fix version date

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@ namespace Nebule\Bootstrap;
 const BOOTSTRAP_NAME = 'bootstrap';
 const BOOTSTRAP_SURNAME = 'nebule/bootstrap';
 const BOOTSTRAP_AUTHOR = 'Project nebule';
-const BOOTSTRAP_VERSION = '020210510';
+const BOOTSTRAP_VERSION = '020210607';
 const BOOTSTRAP_LICENCE = 'GNU GPL 02021';
 const BOOTSTRAP_WEBSITE = 'www.nebule.org';
 // ------------------------------------------------------------------------------------------
