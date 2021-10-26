35e68bfce394376dbca6c7fe43a62c1b31030587
nebuleorg <deisto@nebule.org>
2021-10-26 07:47:46 +0200

    add script to prepare dev and test env

---
+++ prep_loop_code_master_php.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Prepare or restore an environment to develop and test code master.
+
+# Author Projet nebule
+# License GNU GPLv3
+# Copyright Projet nebule
+# www.nebule.org
+# Version 020211026
+
