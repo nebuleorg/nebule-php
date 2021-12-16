4c9f433b71d7e33b3c16391ea267017a314852b1
nebuleorg <deisto@nebule.org>
2021-12-16 22:15:21 +0100

    fix syntax

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 namespace Nebule\Bootstrap;
+
 //use nebule;
 // ------------------------------------------------------------------------------------------
 use Nebule\Library\nebule;
--- lib_nebule/301_social.php
+++ lib_nebule/301_social.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 namespace Nebule\Library;
+
 // TODO à refaire plus flexible
 
 /**
