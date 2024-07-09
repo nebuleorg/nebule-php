262946fd54f3cf3c9fd4917be7e7825219f2b367
nebuleorg <deisto@nebule.org>
2024-07-09 18:48:01 +0200

    date

--- lib_nebule/000_init.php
+++ lib_nebule/000_init.php
@@ -9,7 +9,7 @@ $nebuleSurname = 'nebule/library';
 /** @noinspection PhpUnusedLocalVariableInspection */
 $nebuleDescription = 'Library of functions for nebule in php object-oriented.';
 $nebuleAuthor = 'Projet nebule';
-$nebuleLibVersion = '020240708';
+$nebuleLibVersion = '020240709';
 $nebuleLicence = 'GNU GPL 2010-2024';
 $nebuleWebsite = 'www.nebule.org';
 // ----------------------------------------------------------------------------------------
